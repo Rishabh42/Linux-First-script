e7d1923254353eea272a3d78d37eced328fe9001
Rishabh42 <rishabhthaney@gmail.com>
2018-11-20 18:35:40 +0530

    atom, redshift, truffle, gdebi-core, truffle, ganache

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
+
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
 sudo add-apt-repository ppa:noobslab/themes\
@@ -6,6 +7,7 @@ sudo add-apt-repository ppa:deluge-team/ppa
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
 sudo sh -c "echo 'AutoEnable=true' >> /etc/bluetooth/main.conf"
+sudo add-apt-repository ppa:webupd8team/atom
 
 
 sudo apt-get update
@@ -19,3 +21,9 @@ sudo apt-get install arc-icons
 sudo apt install deluge
 sudo apt-get install spotify-client
 sudo apt-get install vlc
+sudo apt-get install redshift redshift-gtk
+sudo apt install atom
+sudo apt install gdebi-core
+
+npm install -g truffle
+npm install -g ganache-cli
