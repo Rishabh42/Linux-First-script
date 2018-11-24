04862ea84372592185aefa72d2bc9003df99af3a
Rishabh42 <rishabhthaney@gmail.com>
2018-11-24 16:06:20 +0530

    Added Brave

--- script.sh
+++ script.sh
@@ -9,6 +9,8 @@ echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sourc
 sudo sh -c "echo 'AutoEnable=true' >> /etc/bluetooth/main.conf"
 sudo add-apt-repository ppa:webupd8team/atom
 wget -c https://zoom.us/client/latest/zoom_amd64.deb
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ `lsb_release -sc` main" | sudo tee /etc/apt/sources.list.d/brave-browser-release-`lsb_release -sc`.list
 
 
 sudo apt-get update
@@ -28,6 +30,7 @@ sudo apt install gdebi-core
 sudo apt install snapd
 sudo snap install slack --classic
 sudo dpkg -i zoom_amd64.deb
+sudo apt install brave-browser brave-keyring
 
 npm install -g truffle
 npm install -g ganache-cli
