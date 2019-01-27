4bb9f20d23992a55acec9d285cfbe76360f0c350
Rishabh Thaney <rishabhthaney@gmail.com>
2019-01-27 23:59:21 +0530

    fixed arc-icons repo

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' | sudo tee /etc/apt/sources.list.d/google-chrome.list
-sudo add-apt-repository ppa:noobslab/themes\
 sudo add-apt-repository ppa:noobslab/themes
+sudo add-apt-repository ppa:noobslab/icons
 sudo add-apt-repository ppa:deluge-team/ppa
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
 echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
